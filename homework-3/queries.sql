abe60c991e169d0e888c3fc3cb7ebbdddc3c5e05
miroshnikoff <taxi83080@gmail.com>
2023-08-09 00:45:42 +0300

    editing queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -9,9 +9,8 @@ FROM (SELECT customer_id, CONCAT(first_name, ' ', last_name) AS employee, city A
      ) AS orders
 JOIN customers USING(customer_id)
 WHERE customers.city = orders.employee_city AND ship_via = (
-                                                            SELECT shipper_id
-                                                            FROM shippers
-															WHERE company_name = 'United Package'
+                                                            SELECT shipper_id FROM shippers
+                                                            WHERE company_name = 'United Package'
 														   )
 
 
